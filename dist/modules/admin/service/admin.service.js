1a4ed2f0176c31ab7e1639841087bfd0e53abe24
Dhirajchaudari <dhirajchaudhari789@gmail.com>
2024-07-31 03:29:34 +0530

    server changes

--- dist/modules/admin/service/admin.service.js
+++ dist/modules/admin/service/admin.service.js
@@ -111,7 +111,7 @@ class AdminService {
                     httpOnly: true,
                     sameSite: "none",
                     secure: true,
-                    domain: `https://online-lecture-scheduler-client.vercel.app/`,
+                    domain: `.vercel.app`,
                     path: "/",
                 });
             }
